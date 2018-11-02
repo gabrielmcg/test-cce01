3b44d2e408ec86e191c467b91e1aa8d163bfad92
gabrielmcg <gabriel.mcgoldrick@hpe.com>
2018-11-02 17:09:22 +0000

    Update puppeteer_example.js

--- tests/k8s-guestbook/puppeteer-test/puppeteer_example.js
+++ tests/k8s-guestbook/puppeteer-test/puppeteer_example.js
@@ -11,36 +11,21 @@ const MESSAGE = process.env.MESSAGE || 'test message';
 await page.type('body > div > form > fieldset > input', MESSAGE );
 await page.click('body > div > form > fieldset > button');
 
-//await page.waitForNavigation({waitUntil: 'networkidle2'});
-
-console.log("back from refresh");
-
-
-
-//await page.goto('http://10.10.174.231:32798');
-
-
 const words= await page.evaluate(() => {
 
 console.log("in page evaluate ");
 
  const messages= Array.from(document.querySelectorAll('body > div > div > div'));
 
-// console.log("messages: " + messages);
-
 return messages.map(msg => {
       const msg_text  = msg.textContent.trim();
 
-//     console.log("msg_text " + msg_text);
      return msg_text;
  });
 });
 
 
   console.log(words.join('\n'));
-
-
-//await page.screenshot({path: 'example.png'});
 
   await browser.close();
 })();
